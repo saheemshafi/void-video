58209cf21bd241ae050f8b3953444d4eec76d2e5
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-02 01:37:12 +0530

    chore: added comment routes

--- apps/server/src/routes/post.routes.ts
+++ apps/server/src/routes/post.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import upload from '../middlewares/multer.middleware';
 import { authorize } from '../middlewares/auth.middleware';
 import {
+  addCommentToPost,
   deletePost,
   getPost,
   getPostComments,
@@ -15,8 +16,9 @@ const postRouter = Router();
 postRouter.route('/').post(authorize, upload.array('images', 4), uploadPost);
 postRouter.route('/:postId').get(getPost);
 postRouter.route('/:postId/like').get(authorize, likePost);
-postRouter.route('/:postId/comments').get(getPostComments);
-postRouter.route('/:postId').put(authorize, updatePost);
+postRouter.route('/:postId').patch(authorize, updatePost);
 postRouter.route('/:postId').delete(authorize, deletePost);
+postRouter.route('/:postId/comments').get(getPostComments);
+postRouter.route('/:postId/comments').post(authorize, addCommentToPost);
 
 export default postRouter;
