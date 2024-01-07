00a33b37aed85c16a82c13e26ffb10f6f1d88a10
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-07 14:12:55 +0530

    feat: create and remove subscription

--- apps/server/src/controllers/subscription.controller.ts
+++ apps/server/src/controllers/subscription.controller.ts
@@ -1,4 +1,9 @@
+import { STATUS_CODES } from '../constants';
+import { Subscription } from '../models/subscription.model';
+import { User } from '../models/user.model';
 import { validateRequest } from '../utils';
+import ApiError from '../utils/api-error';
+import ApiResponse from '../utils/api-response';
 import asyncHandler from '../utils/async-handler';
 import {
   createSubscriptionValidation,
@@ -9,12 +14,59 @@ const createSubscription = asyncHandler(async (req, res) => {
   const {
     body: { channelId },
   } = validateRequest(req, createSubscriptionValidation);
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) {
+    throw new ApiError(STATUS_CODES.NOT_FOUND, 'Channel not found.');
+  }
+
+  const subscription = await Subscription.create({
+    channel: channel._id,
+    subscriber: req.user?._id,
+  });
+
+  if (!subscription) {
+    throw new ApiError(
+      STATUS_CODES.INTERNAL_SERVER_ERROR,
+      'Subscription failed.'
+    );
+  }
+
+  res
+    .status(STATUS_CODES.OK)
+    .json(
+      new ApiResponse(STATUS_CODES.OK, 'Subscription added.', subscription)
+    );
 });
 
 const removeSubscription = asyncHandler(async (req, res) => {
   const {
     body: { channelId },
   } = validateRequest(req, removeSubscriptionValidation);
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) {
+    throw new ApiError(STATUS_CODES.NOT_FOUND, 'Channel not found.');
+  }
+
+  const subscription = await Subscription.findOne({
+    channel: channelId,
+    subscriber: req.user?._id,
+  });
+
+  if (!subscription) {
+    throw new ApiError(STATUS_CODES.NOT_FOUND, 'Subscription not found.');
+  }
+
+  await subscription.deleteOne();
+
+  res
+    .status(STATUS_CODES.OK)
+    .json(
+      new ApiResponse(STATUS_CODES.OK, 'Removed subscription.', subscription)
+    );
 });
 
 export { createSubscription, removeSubscription };
