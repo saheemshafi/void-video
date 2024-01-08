f9b25d9fa5ef957f64f1158eb8bc1137b2558ef9
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-09 02:01:29 +0530

    refactor: post routes

--- apps/server/src/routes/post.routes.ts
+++ apps/server/src/routes/post.routes.ts
@@ -15,14 +15,24 @@ import {
 
 const postRouter = Router();
 
-postRouter.route('/').post(authorize, upload.array('images', 4), uploadPost);
-postRouter.route('/').get(getPosts);
-postRouter.route('/:postId').get(getPost);
+postRouter
+  .route('/')
+  .post(authorize, upload.array('images', 4), uploadPost)
+  .get(getPosts);
+
+postRouter
+  .route('/:postId')
+  .get(getPost)
+  .patch(authorize, updatePost)
+  .delete(authorize, deletePost);
+
 postRouter.route('/:postId/like').get(authorize, likePost);
-postRouter.route('/:postId').patch(authorize, updatePost);
-postRouter.route('/:postId').delete(authorize, deletePost);
-postRouter.route('/:postId/comments').get(getPostComments);
-postRouter.route('/:postId/comments').post(authorize, addCommentToPost);
+
+postRouter
+  .route('/:postId/comments')
+  .get(getPostComments)
+  .post(authorize, addCommentToPost);
+
 postRouter
   .route('/:postId/change-images')
   .patch(authorize, upload.array('images', 4), changePostImages);
