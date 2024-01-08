d1a9f56c2ee0cf28bda3b0679ff264a342b31b4e
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-09 02:10:21 +0530

    chore: add comment routes, controller and validation files

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -4,6 +4,7 @@ import postRouter from './post.routes';
 import videoRouter from './video.routes';
 import subscriptionRouter from './subscription.routes';
 import playlistRouter from './playlist.routes';
+import commentRouter from './comment.routes';
 
 const router = Router();
 router.get('/', (_, res) => {
@@ -15,5 +16,6 @@ router.use('/videos', videoRouter);
 router.use('/posts', postRouter);
 router.use('/subscriptions', subscriptionRouter);
 router.use('/playlists', playlistRouter);
+router.use('/comments', commentRouter);
 
 export default router;
