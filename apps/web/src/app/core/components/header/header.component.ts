31ed94885c61e2ed97ab6acd09144c6c96043aed
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-15 04:41:00 +0530

    chore: conditionally display authentication buttons

--- apps/web/src/app/core/components/header/header.component.ts
+++ apps/web/src/app/core/components/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private authService = inject(AuthService);
 
+  session$ = this.authService.session$;
 }
--- apps/web/src/app/core/pages/home/home.component.ts
+++ apps/web/src/app/core/pages/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  private authService = inject(AuthService);
+  session$ = this.authService.session$;
 
+  constructor() {
+    this.session$ = this.authService.session$;
+  }
+  
 }
