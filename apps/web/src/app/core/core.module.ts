eb231e9eedf1c22969d42762b7e7e6709733d302
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-16 03:28:19 +0530

    feat: made sidebar keyboard accessible with angular cdk menu

--- apps/web/src/app/core/core.module.ts
+++ apps/web/src/app/core/core.module.ts
@@ -7,8 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
 
+import { CdkMenuModule } from '@angular/cdk/menu';
+
 @NgModule({
-  declarations: [CoreComponent, HeaderComponent, SidebarComponent, HomeComponent],
-  imports: [CommonModule, CoreRoutingModule, NgOptimizedImage],
+  declarations: [
+    CoreComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent,
+  ],
+  imports: [CommonModule, CoreRoutingModule, NgOptimizedImage, CdkMenuModule],
 })
 export class CoreModule {}
