f7838765f999ba4aaaab7b8f1c16252198d0f447
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-17 22:20:54 +0530

    feat: theme service

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import {
   BrowserModule,
   provideClientHydration,
@@ -7,11 +7,20 @@ import {
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeService } from './shared/services/theme.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [provideClientHydration(), provideHttpClient(withFetch())],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      deps: [ThemeService],
+      useFactory: () => {},
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
