5eebb7a7b46b827a34981a7f8d1a0f14b6f8f2d4
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-17 08:36:30 +0530

    chore: extracted profile menu header is separate component

--- apps/web/src/app/shared/components/profile-menu/user-profile/user-profile.component.ts
+++ apps/web/src/app/shared/components/profile-menu/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
--- apps/web/src/app/shared/shared.module.ts
+++ apps/web/src/app/shared/shared.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SvgLoaderComponent } from './components/svg-loader/svg-loader.component';
 import { FieldErrorComponent } from './components/field-error/field-error.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { UserProfileComponent } from './components/profile-menu/user-profile/user-profile.component';
 import { CdkMenuModule } from '@angular/cdk/menu';
 import { RouterModule } from '@angular/router';
-import { AppearanceMenuComponent } from './components/appearance-menu/appearance-menu.component';
+import { AppearanceMenuComponent } from './components/profile-menu/appearance-menu/appearance-menu.component';
+import { ProfileMenuHeaderComponent } from './components/profile-menu/profile-menu-header/profile-menu-header.component';
 
 @NgModule({
-  declarations: [SvgLoaderComponent, FieldErrorComponent, UserProfileComponent, AppearanceMenuComponent],
+  declarations: [
+    SvgLoaderComponent,
+    FieldErrorComponent,
+    UserProfileComponent,
+    AppearanceMenuComponent,
+    ProfileMenuHeaderComponent,
+  ],
   imports: [CommonModule, CdkMenuModule, RouterModule],
   exports: [SvgLoaderComponent, FieldErrorComponent, UserProfileComponent],
 })
