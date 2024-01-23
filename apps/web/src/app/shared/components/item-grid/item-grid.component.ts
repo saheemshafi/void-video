24c9a33bf2d9e06d6ec41f49b1bd2d1ddfa10a95
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-23 08:40:46 +0530

    chore: add item grid component

---
+++ apps/web/src/app/shared/components/item-grid/item-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-grid',
+  templateUrl: './item-grid.component.html',
+  styleUrl: './item-grid.component.scss'
+})
+export class ItemGridComponent {
+
+}
