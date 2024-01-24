ae91840ebee2224146c1dc64d4043dce4703cc56
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-24 09:39:04 +0530

    fix: get video on client side to send cookies along

--- apps/web/src/app/core/pages/watch/watch.component.ts
+++ apps/web/src/app/core/pages/watch/watch.component.ts
@@ -1,22 +1,30 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
-import { VideoService } from '../../../shared/services/video.service';
-import { ActivatedRoute } from '@angular/router';
-import { Observable, map, of, switchMap, tap } from 'rxjs';
+import { isPlatformServer } from '@angular/common';
+import {
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  PLATFORM_ID,
+  inject,
+} from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Video } from '../../../shared/interfaces/video';
+import { VideoService } from '../../../shared/services/video.service';
 
 @Component({
   selector: 'app-watch',
   templateUrl: './watch.component.html',
   styleUrl: './watch.component.scss',
 })
-export class WatchComponent {
+export class WatchComponent implements OnInit {
   private videoService = inject(VideoService);
-
   @Input() videoId: string = '';
-
   video$: Observable<Video> = of();
 
-  ngOnInit() {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformServer(this.platformId)) return;
     this.video$ = this.videoService.getVideo(this.videoId);
   }
 }
