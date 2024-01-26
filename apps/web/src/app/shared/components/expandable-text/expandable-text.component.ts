0b668bfecd6acfe96911b67d8a3995cd4b5455a4
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-27 01:55:59 +0530

    refactor: implement on init interface

--- apps/web/src/app/shared/components/expandable-text/expandable-text.component.ts
+++ apps/web/src/app/shared/components/expandable-text/expandable-text.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-expandable-text',
   templateUrl: './expandable-text.component.html',
   styleUrl: './expandable-text.component.scss',
 })
-export class ExpandableTextComponent {
+export class ExpandableTextComponent implements OnInit {
   @Input({ required: true }) text: string = '';
   @Input() maxLength: number = 200;
   @Input({ required: true }) id: string = '';
@@ -13,7 +13,7 @@ export class ExpandableTextComponent {
   collapsedText: string = '';
   canExpand: boolean = false;
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.text.length > this.maxLength) {
       this.canExpand = true;
       this.collapsedText = this.text.slice(0, this.maxLength);
