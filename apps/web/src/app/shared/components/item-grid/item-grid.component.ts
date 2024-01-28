8f273a2ba10bf5b658f2a51c8022d63aae6a150d
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-01-29 02:27:20 +0530

    refactor: accept column values as inputs

--- apps/web/src/app/shared/components/item-grid/item-grid.component.ts
+++ apps/web/src/app/shared/components/item-grid/item-grid.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+type GridColumns = {
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+};
 
 @Component({
   selector: 'app-item-grid',
   templateUrl: './item-grid.component.html',
-  styleUrl: './item-grid.component.scss'
+  styleUrl: './item-grid.component.scss',
 })
 export class ItemGridComponent {
+  @Input() columns: Partial<GridColumns> = {};
 
+  protected readonly DEFAULT_COLUMNS: GridColumns = {
+    sm: 1,
+    md: 2,
+    lg: 2,
+    xl: 3,
+  };
 }
