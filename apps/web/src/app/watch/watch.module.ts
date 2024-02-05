4dfa3489dc1c07ed48ad44e2243bbe3918b37972
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-02-05 18:56:00 +0530

    refactor: rename watch-page-channel-bar to channel-bar

--- apps/web/src/app/watch/watch.module.ts
+++ apps/web/src/app/watch/watch.module.ts
@@ -4,20 +4,20 @@ import { CommonModule } from '@angular/common';
 import { WatchRoutingModule } from '~watch/watch-routing.module';
 import { WatchComponent } from '~watch/watch.component';
 import { VideoPlayerComponent } from '~watch/components/video-player/video-player.component';
-import { WatchPageChannelBarComponent } from '~watch/components/watch-page-channel-bar/watch-page-channel-bar.component';
+import { ChannelBarComponent } from '~watch/components/channel-bar/channel-bar.component';
 import { WatchPageComponent } from '~watch/pages/watch/watch-page.component';
+import { LikeButtonComponent } from '~watch/components/like-button/like-button.component';
+import { ShareButtonComponent } from '~watch/components/share-button/share-button.component';
+import { CommentBoxComponent } from '~watch/components/comment-box/comment-box.component';
+import { CommentFormComponent } from '~watch/components/comment-form/comment-form.component';
 
 import { SharedModule } from '~shared/shared.module';
-import { LikeButtonComponent } from './components/like-button/like-button.component';
-import { ShareButtonComponent } from './components/share-button/share-button.component';
-import { CommentBoxComponent } from './components/comment-box/comment-box.component';
-import { CommentFormComponent } from './components/comment-form/comment-form.component';
 
 @NgModule({
   declarations: [
     WatchComponent,
     VideoPlayerComponent,
-    WatchPageChannelBarComponent,
+    ChannelBarComponent,
     WatchPageComponent,
     LikeButtonComponent,
     ShareButtonComponent,
