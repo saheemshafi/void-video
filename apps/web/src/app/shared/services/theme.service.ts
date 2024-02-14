78d3434c155b2132e250ddaa46a5828d770e54e3
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-02-14 15:11:36 +0530

    chore: store theme in local storage

--- apps/web/src/app/shared/services/theme.service.ts
+++ apps/web/src/app/shared/services/theme.service.ts
@@ -14,7 +14,7 @@ export type Theme = 'system' | 'dark' | 'light';
   providedIn: 'root',
 })
 export class ThemeService implements OnDestroy {
-  private themeSubject = new BehaviorSubject<Theme>('system');
+  private themeSubject = new BehaviorSubject<Theme | null>(null);
   private themeSubscription!: Subscription;
   private platformId = inject(PLATFORM_ID);
 
@@ -23,12 +23,17 @@ export class ThemeService implements OnDestroy {
   constructor() {
     if (isPlatformServer(this.platformId)) return;
 
+    const theme = localStorage.getItem('theme');
+    this.themeSubject.next(<Theme>theme || 'system');
+
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
     this.themeSubscription = combineLatest([
       fromEvent(mediaQuery, 'change').pipe(startWith(null)),
       this.themeSubject,
     ]).subscribe(([_, theme]) => {
+      if (!theme) return;
+
       if (theme == 'system') {
         document.documentElement.className = mediaQuery.matches ? 'dark' : '';
         return;
@@ -40,6 +45,7 @@ export class ThemeService implements OnDestroy {
 
   setTheme(theme: Theme): void {
     this.themeSubject.next(theme);
+    localStorage.setItem('theme', theme);
   }
 
   ngOnDestroy(): void {
