0fb0103d2b1bc84e6ec3b9ca667cb67f5fac159f
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-02-15 17:08:11 +0530

    chore: disable comment button

--- apps/web/src/app/watch/components/comment-form/comment-form.component.ts
+++ apps/web/src/app/watch/components/comment-form/comment-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  inject,
+} from '@angular/core';
 import { FormBuilder, NgForm, Validators } from '@angular/forms';
 import { BehaviorSubject, Subject, take } from 'rxjs';
 
@@ -9,14 +17,16 @@ import { VideoService } from '~shared/services/video.service';
   selector: 'app-comment-form',
   templateUrl: './comment-form.component.html',
   styleUrl: './comment-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentFormComponent {
   @Input({ required: true }) videoId: string = '';
   private fb = inject(FormBuilder);
   private videoService = inject(VideoService);
+  private cdr = inject(ChangeDetectorRef);
   @Output() commentAdded = new EventEmitter<Comment>();
 
-  submitting: true | undefined = undefined;
+  submitting = false;
 
   commentForm = this.fb.group({
     comment: this.fb.control('', {
@@ -28,6 +38,8 @@ export class CommentFormComponent {
     if (this.commentForm.invalid) return;
 
     this.submitting = true;
+    // Async pipe not triggering change detection for some reason. CONFIRMATION NEEDED
+    this.cdr.detectChanges();
 
     this.videoService
       .comment(this.videoId, this.commentForm.value?.comment || '')
@@ -39,6 +51,6 @@ export class CommentFormComponent {
         },
       });
 
-    this.submitting = undefined;
+    this.submitting = false;
   }
 }
