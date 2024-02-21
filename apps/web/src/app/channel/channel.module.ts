3996414f8693e4e54112a38374dea5682e8356e2
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-02-22 02:10:46 +0530

    chore: change avatar

--- apps/web/src/app/channel/channel.module.ts
+++ apps/web/src/app/channel/channel.module.ts
@@ -8,6 +8,7 @@ import { PlaylistsPageComponent } from '~channel/pages/playlists-page/playlists-
 import { VideosPageComponent } from '~channel/pages/videos-page/videos-page.component';
 import { SharedModule } from '~shared/shared.module';
 import { ChannelNavComponent } from './components/channel-nav/channel-nav.component';
+import { ChannelAvatarComponent } from './components/channel-avatar/channel-avatar.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ChannelNavComponent } from './components/channel-nav/channel-nav.compon
     PlaylistsPageComponent,
     AboutPageComponent,
     ChannelNavComponent,
+    ChannelAvatarComponent,
   ],
   imports: [CommonModule, ChannelRoutingModule, SharedModule],
 })
