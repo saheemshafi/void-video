45faf2214265cee2b501556674938b51f3d19eda
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-02-21 21:19:25 +0530

    chore: cache user session and channel

--- apps/server/src/middlewares/auth.middleware.ts
+++ apps/server/src/middlewares/auth.middleware.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 import ApiError from '../utils/api-error';
-import { User } from '../models/user.model';
+import { IUser, User } from '../models/user.model';
 import { NextFunction, Request, Response } from 'express';
 import { STATUS_CODES } from '../constants';
+import redis from '../db/redis';
 
 export const authorize =
   (ignoreUnauthorized: boolean = false) =>
@@ -21,15 +22,24 @@ export const authorize =
         process.env.ACCESS_TOKEN_SECRET as string
       );
 
-      const user = await User.findById(
-        typeof verifiedToken !== 'string' && verifiedToken.id
-      );
+      const userId = typeof verifiedToken !== 'string' && verifiedToken.id;
+
+      const cachedUser = await redis.get(`session:${userId}`);
+
+      let user: undefined | null | IUser;
+
+      if (cachedUser) {
+        user = JSON.parse(cachedUser);
+      } else {
+        user = await User.findById(userId);
+        redis.set(`session:${userId}`, JSON.stringify(user));
+      }
 
       if (!user) {
         throw new ApiError(STATUS_CODES.UNAUTHORIZED, 'Unauthorized.');
       }
 
-      req.user = user || undefined;
+      req.user = user;
       next();
     } catch (error) {
       if (ignoreUnauthorized) return next();
